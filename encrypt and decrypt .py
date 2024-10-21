f99dcb70d616442c0bd563cf3e8146ae39d498ae
sumanth37z <sumanth7077@gmail.com>
2024-10-21 19:04:34 +0530

    Add files via upload
    This program allows users to easily encrypt and decrypt text using a simple substitution cipher. Just input your message and a key to scramble or unscramble your text, making it a perfect introduction to basic encryption.

---
+++ encrypt and decrypt .py
@@ -0,0 +1,37 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
+#print(f"chars : {chars}")
+#print(f"key : {key}")
+
+#To Encrypt
+
+text = input("Enter the message to encrypt: ")
+cipher_code = ""
+
+for letter in text:
+    index = chars.index(letter)
+    cipher_code += key[index]
+
+print(f"Original message : {text}")
+print(f"Encrypted message: {cipher_code}")
+
+
+#To Decrypt
+
+cipher_code = input("Enter the message to encrypt: ")
+text = ""
+
+for letter in cipher_code:
+    index = key.index(letter)
+    text += chars[index]
+
+
+print(f"Encrypted message: {cipher_code}")
+print(f"Original message : {text}")
